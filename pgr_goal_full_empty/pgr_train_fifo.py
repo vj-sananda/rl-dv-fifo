Fixed Policy gradient example Added sram-multi-counter example to demonstrate state encoding using past actions

pgr_goal_full_empty/pgr_train_fifo.py
@@ -15,6 +15,8 @@ import dill
 import sys
 from pprint import pprint as pp
 
+from rl_fifo_utils import get_state_reward, tick, reset, get_state_reward, Switcher, ACTION_NAME_MAP
+
 import torch
 import torch.optim as optim
 from policy_network import Policy
@@ -38,12 +40,6 @@ async def train_fifo(dut):
 
     GAMMA = 1.0
 
-    EPS_START = 1.0
-
-    EPS_DECAY = 0.999
-
-    EPS_MIN = 0.05
-
     TARGET_SCORE = 10000
 
     #Size of Action space = 4 : {pop, push}
@@ -58,15 +54,6 @@ async def train_fifo(dut):
     if 'ALPHA' in cocotb.plusargs:
         ALPHA = float(cocotb.plusargs['ALPHA'])
 
-    if 'EPS_DECAY' in cocotb.plusargs:
-        EPS_DECAY = float(cocotb.plusargs['EPS_DECAY'])
-
-    if 'EPS_MIN' in cocotb.plusargs:
-        EPS_MIN = float(cocotb.plusargs['EPS_MIN'])
-
-    if 'EPS_START' in cocotb.plusargs:
-        EPS_START = float(cocotb.plusargs['EPS_START'])
-
     if 'GAMMA' in cocotb.plusargs:
         GAMMA = float(cocotb.plusargs['GAMMA'])
 
@@ -75,92 +62,6 @@ async def train_fifo(dut):
 
     for k,v in cocotb.plusargs.items():
         print("{} = {}".format(k,v))
-
-    async def tick(n = 1):
-        for _ in range(n):
-            await RisingEdge(dut.clk)
-
-    async def reset():
-        #dut._log.info("Start reset")
-        dut.rst <= 1 #Assert rst
-        dut.push <= 0
-        dut.pop <= 0
-        await tick(5)
-        dut.rst <= 0 #deassert rst
-        await tick(5)
-        #dut._log.info("End reset")
-
-
-    def compute_reward(filled):
-        reward = -1
-        next_filled = filled
-        if dut.full == 1 and filled == 0:
-            reward = 100
-            next_filled = 1
-        elif dut.empty == 1 and filled == 1:
-            reward = 100
-            next_filled = 0
-        return reward,next_filled
-
-    """
-    def compute_reward(select):
-        reward = -1
-        next_select = select
-        if dut.full_posedge == 1:
-            reward = 100
-        return reward,next_select
-    """
-
-    """
-    def compute_reward(select):
-        reward = -1
-        next_select = select
-        if dut.empty_posedge == 1:
-            reward = 100
-        return reward,next_select
-    """
-
-    def get_state_reward(select):
-        reward,next_select = compute_reward(select)
-        next_state = (next_select, dut.count.value.integer, dut.empty.value.integer, dut.full.value.integer)
-        return next_state,reward,next_select
-
-    #Case like statement to translate action to {pop,push}
-    class Switcher(object):
-        def do_action(self,i):
-            method_name='action_'+str(i)
-            method=getattr(self,method_name,lambda :'Invalid')
-            return method()
-
-        def action_0(self):
-            dut.push <= 0
-            dut.pop <= 0
-
-        def action_1(self):
-            if dut.full == 0:
-                dut.push <= 1
-            else:
-                dut.push <= 0
-            dut.pop <= 0
-
-        def action_2(self):
-            if dut.empty == 0:
-                dut.pop <= 1
-            else:
-                dut.pop <= 0
-            dut.push <= 0
-
-        def action_3(self):
-            if dut.full == 0:
-                dut.push <= 1
-            else:
-                dut.push <= 0
-            if dut.empty == 0:
-                dut.pop <= 1
-            else:
-                dut.pop <= 0
-
-    ACTION_NAME_MAP = { 0: 'NONE', 1:'PUSH' , 2 : 'POP', 3:'BOTH'}
 
     async def reinforce(gamma=0.999, print_every=100):
         """
@@ -187,14 +88,11 @@ async def train_fifo(dut):
             score = 0                                              # initialize score
 
             #state = env.reset()                                    # start episode
-            await reset()
+            await reset(dut)
 
             total_episode_reward = 0
-            select = 0
-            dut.select = select
-            state, reward, next_select = get_state_reward(select)
+            state, reward = get_state_reward(dut)
             #dut._log.info("Reset state = {}".format(state))
-            select = next_select
             dut.reward = reward
 
             for _ in range(EPISODE_LENGTH):
@@ -202,15 +100,14 @@ async def train_fifo(dut):
                 action, log_prob = policy.act(np.array(state,dtype=np.float32))
                 saved_log_probs.append(log_prob)
 
-                s=Switcher()
+                s=Switcher(dut)
                 s.do_action(action)
                 #print("state:{} + action:{} ->".format(state,action),end="")
 
-                await tick()
+                await tick(dut)
 
-                next_state,reward,next_select = get_state_reward(select)
+                next_state,reward = get_state_reward(dut)
                 #print("next_state:{}".format(next_state))
-                dut.select <= select
                 dut.reward <= reward
 
                 done = False
@@ -218,7 +115,6 @@ async def train_fifo(dut):
                 rewards.append(reward)
 
                 state = next_state
-                select = next_select
 
             scores_deque.append(sum(rewards))
             scores.append(sum(rewards))

sram-multi-counter/policy.py
@@ -10,15 +10,11 @@ from cocotb.triggers import RisingEdge
 from cocotb.triggers import Join
 import numpy as np
 from collections import defaultdict
-from collections import deque
 import dill
 import sys
 from pprint import pprint as pp
-import torch
-import torch.optim as optim
-from policy_network import Policy
 
-from rl_fifo_utils import get_state_reward, tick, reset, get_state_reward, Switcher, ACTION_NAME_MAP
+from rl_utils import get_state_reward, tick, reset, get_state_reward, Switcher, ACTION_NAME_MAP, initialize_counters, initialize_inputs
 
 #Length of an episode in timesteps
 EPISODE_LENGTH = 200
@@ -26,28 +22,32 @@ EPISODE_LENGTH = 200
 #Number of episodes over which to train
 NUM_EPISODES = 1000
 
-#Size of Action space = 4 : {pop, push}
-nA = 4
-
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
-policy = Policy().to(device)
+#Size of Action space = 6
+nA = 6
 
 @cocotb.test()
-async def policy_fifo(dut):
-    """ Test to that runs policy found by training """
+async def policy(dut):
+    """ Test that runs policy found by training """
 
-    async def run_policy(agent):
+
+    async def run_policy(policy,nA):
         """ run simulation based on policy """
 
         total_reward = 0
+        await initialize_inputs(dut)
         await reset(dut)
+        await initialize_counters(dut)
 
         state, reward = get_state_reward(dut)
- 
+
         for _ in range(EPISODE_LENGTH):
 
-            action,_ = agent.act(np.array(state,dtype=np.float32))
+            if state in policy:
+                action = policy[state]
+                dut._log.info("For state:{} => {}".format(state,ACTION_NAME_MAP[action]))
+            else:
+                dut._log.error("No action found in policy for state {}".format(state))
+                action = np.random.choice(np.arange(nA))
 
             s=Switcher(dut)
             s.do_action(action)
@@ -72,12 +72,15 @@ async def policy_fifo(dut):
     2 ps clock :  2.61 sec
     2 ns clock : 17.14 sec
     """
-    clock = Clock(dut.clk, 2, units="ns")
+    clock = Clock(dut.clk, 10, units="ns")
     cocotb.fork(clock.start())  # Start the clock
 
-    #Load weights of trained policy network
-    policy.load_state_dict(torch.load('checkpoint.pth'))
+    with open("policy.dump","rb") as f:
+        (policy,Q) = dill.load(f)
 
-    total_reward = await run_policy(policy)
+    pp(policy)
+    pp(Q)
+
+    total_reward = await run_policy(policy,nA)
 
     dut._log.info('Total reward = {}'.format(total_reward))